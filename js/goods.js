4a8990a4c32882b54a51d68a460ee3a56943722f
Войтик Борис <borisvoitik@gmail.com>
2018-10-07 21:51:12 +0300

    Выполненное 4ое задание

--- js/goods.js
+++ js/goods.js
@@ -127,6 +127,7 @@ var getGoods = function () {
 getGoods();
 
 
+
 var uselessCatalogCards = document.querySelector('.catalog__cards--load');
 var uselessCatalogLoad = document.querySelector('.catalog__load');
 
